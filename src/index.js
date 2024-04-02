b2803618fbe359b4cf219060868b7887ffa96fd9
subhransuO01 <subhransu.das001@outlook.com>
2024-04-02 17:11:22 +0530

    added some component into single file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
+
+import { Component } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+function A (props){
+ let firstName="Bhran";
+ let rollno = "1";
+return <h1>First Name is:{firstName} <B rollno={rollno}/> Surname is:{props.surname} </h1>
+}
+
+class B extends Component{
+  middleName ="Sekhar";
+  render(){
+    return <h1>Middle name is :{this.middleName} and rollno is: {this.props.rollno} company is:<C>Harman</C></h1>
+  }
+}
+
+let C = (props)=>{
+  console.log(props);
+  return <span>{props.children}</span> //console the props response then we will get to know how to get props.children
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+let title="Das";
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <A surname={title}/>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
