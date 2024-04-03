795be6504d43760d1ddfa0f953507b61a4331fb3
subhransuO01 <subhransu.das001@outlook.com>
2024-04-03 08:10:05 +0530

    added component with various access logic into single file

--- src/index.js
+++ src/index.js
@@ -6,18 +6,19 @@ import ReactDOM from 'react-dom/client';
 function A (props){
  let firstName="Bhran";
  let rollno = "1";
-return <h1>First Name is:{firstName} <B rollno={rollno}/> Surname is:{props.surname} </h1>
+return <span>First Name is:{firstName} <B rollno={rollno}>Athgarh</B> Surname is:{props.surname} </span>
 }
 
 class B extends Component{
   middleName ="Sekhar";
   render(){
-    return <h1>Middle name is :{this.middleName} and rollno is: {this.props.rollno} company is:<C>Harman</C></h1>
+    //console.log(this.props);
+    return <h1>Middle name is :{this.middleName} and rollno is: {this.props.rollno} Address is:{this.props.children} company is:<C>Harman</C></h1>
   }
 }
 
 let C = (props)=>{
-  console.log(props);
+  //console.log(props);
   return <span>{props.children}</span> //console the props response then we will get to know how to get props.children
 }
 
